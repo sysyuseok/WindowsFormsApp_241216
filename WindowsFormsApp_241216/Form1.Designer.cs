41277d6bdb601f285b1d5b8259dab8e56a748b3f
sysyuseok <sonyous0513@naver.com>
2024-12-17 11:43:51 +0900

    Add project files.

---
+++ WindowsFormsApp_241216/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp_241216
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_print = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gulim", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(39, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Result : ";
+            // 
+            // textBox_print
+            // 
+            this.textBox_print.Location = new System.Drawing.Point(106, 26);
+            this.textBox_print.Multiline = true;
+            this.textBox_print.Name = "textBox_print";
+            this.textBox_print.Size = new System.Drawing.Size(429, 231);
+            this.textBox_print.TabIndex = 1;
+            this.textBox_print.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_print);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_print;
+    }
+}
+
---
+++ WindowsFormsApp_241216/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_241216
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] arr = new string[10];
+            arr[0] = "동해 물과 백두산이".IndexOf("백두산").ToString();
+            arr[1] = "토요일에 먹는 토마토".LastIndexOf("토").ToString();
+            arr[2] = "질서 있는 퇴장".Contains("퇴").ToString();
+            arr[3]="그 사람의 그림자는 그랬다".Replace("그","이");
+            arr[4]="삼성 갤럭시".Insert("삼성 갤럭시".IndexOf("갤"),"애플 ");
+            arr[5]="오늘은 왠지 더 배고프다".Remove("오늘은 왠지 더 배고프다".IndexOf("더"),2);
+            string[] temp = "이름,나이,전화번호".Split(',');
+            int idx = 0;
+            foreach(string a in temp)
+            {
+                arr[6 + idx] = a;
+                idx++;
+            }
+            arr[9]="우리 나라 만세".Substring(3).Replace(" 만세","");
+            foreach (string a in arr)
+            {
+                System.Console.WriteLine(a);
+                textBox_print.Text += a;
+                textBox_print.Text += "\r\n";
+            }
+        }
+
+    }
+}
