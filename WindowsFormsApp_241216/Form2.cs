413cdcafaaf3006cc88ad9db692c124d13094f18
sysyuseok <sonyous0513@naver.com>
2024-12-20 16:57:47 +0900

    rock scissors paper game

--- WindowsFormsApp_241216/Form2.cs
+++ WindowsFormsApp_241216/Form2.cs
@@ -1,87 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp_241216
 {
     public partial class Form2 : Form
     {
+        int MyScore = 0, ComScore = 0;
+
         public Form2()
         {
             InitializeComponent();
         }
 
+        private void PrintRockScissorsPaper(int my, int com)
+        {
+            string m, y;
+            if (my == 0)
+                m = "ROCK";
+            else if (my == 1)
+                m = "SCISSORS";
+            else m="PAPER";
+            if (com == 0)
+                y = "ROCK";
+            else if (com == 1)
+                y = "SCISSORS";
+            else y="PAPER";
+            textBox_print.Text = ($"ME: {m}  COM: {y}\r\n");
+        }
 
-        private void button_rock_Click(object sender, EventArgs e)
+        private void GamePlayPrintResult(int n)
         {
             Random random = new Random();
             int randomNumber = random.Next(0, 3);
-            textBox_print.Text = randomNumber.ToString();
-            if (randomNumber == 0)
+            PrintRockScissorsPaper(n, randomNumber);
+
+            if (n == randomNumber)
             {
                 textBox_print.Text += "Draw";
             }
+            else if ((n + 1) % 3 == randomNumber)
+            {
+                textBox_print.Text += "WIN";
+                MyScore++;
+            }
             else
             {
-                if (randomNumber == 1)
-                {
-                    textBox_print.Text += "Win";
-                }
-                else
-                {
-                    textBox_print.Text += "Lose";
-                }
+                textBox_print.Text += "LOSE";
+                ComScore++;
             }
+
+            textBox_print.Text += ($"\r\nYour Score: {MyScore}  Computer Score: {ComScore}\r\n");
+
+            CheckGameOver();
+        }
+
+        private void CheckGameOver()
+        {
+            if (MyScore == 3 || ComScore == 3)
+            {
+                string result = MyScore == 3 ? "\r\nYou Win!!!" : "\r\nComputer Wins...";
+                textBox_print.Text += result;
+
+                // Disable buttons to prevent further input
+                button_rock.Enabled = false;
+                button_scissor.Enabled = false;
+                button_paper.Enabled = false;
+            }
+        }
+
+        private void button_rock_Click(object sender, EventArgs e)
+        {
+            GamePlayPrintResult(0);
         }
 
         private void button_scissor_Click(object sender, EventArgs e)
         {
-            Random random = new Random();
-            int randomNumber = random.Next(0, 3);
-            textBox_print.Text = randomNumber.ToString();
-            if (randomNumber == 1)
-            {
-                textBox_print.Text += "Draw";
-            }
-            else
-            {
-                if (randomNumber == 2)
-                {
-                    textBox_print.Text += "Win";
-                }
-                else
-                {
-                    textBox_print.Text += "Lose";
-                }
-            }
+            GamePlayPrintResult(1);
         }
 
         private void button_paper_Click(object sender, EventArgs e)
         {
-            Random random = new Random();
-            int randomNumber = random.Next(0, 3);
-            textBox_print.Text = randomNumber.ToString();
-            if (randomNumber == 2)
-            {
-                textBox_print.Text += "Draw";
-            }
-            else
-            {
-                if (randomNumber == 0)
-                {
-                    textBox_print.Text += "Win";
-                }
-                else
-                {
-                    textBox_print.Text += "Lose";
-                }
-            }
+            GamePlayPrintResult(2);
         }
     }
 }
